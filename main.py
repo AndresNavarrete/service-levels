95846ca8c66f111e25a4a4e562042d22aac7a1d6
Andrés <42160704+AndresNavarrete@users.noreply.github.com>
2022-06-07 13:08:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from packages.serviceLevelManager import ServiceLevelManager
 
 if __name__ == '__main__':
     path = "resources/request.xlsx"
-    apiKey = "AIzaSyCDEBZ-YilN6CRFnPoiiVJaKk_6VXZNiBA"
+    apiKey = "AIzaSyCDEBZ-YilN6CRFnPoiiVJaKk_6VXZNiBA" # This key is depricated. You must place your own key.
     app = ServiceLevelManager(path, apiKey)
     app.execute()
